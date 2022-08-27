dc8951c92f903e852a7c95961ab6154fe9273c66
Maxime <maxime.daraize@gmail.com>
2022-08-27 14:57:12 -0400

    feat: add 0 before single digit number

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -36,7 +36,7 @@ onMounted(() => {
 
 <template>
 <div v-if="Date.parse(date) > Date.parse(new Date)">
-    <div v-for="(time, index) in timeArray" :key="index">{{time}}</div>
+    <div v-for="(time, index) in timeArray" :key="index">{{time.toString().length === 2 ? time : `0${time}`}}</div>
 </div>
 </template>
 
