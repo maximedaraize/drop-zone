21d523911cc4f088ee54f86efc2ec4f637a03a41
Maxime <maxime.daraize@gmail.com>
2022-08-27 14:27:55 -0400

    feat: init time variables

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -1,12 +1,19 @@
 <script setup>
-import { ref } from 'vue'
+import {ref } from "vue";
+
+// first match of the season (habs vs toronto)
+const date = ref(new Date("10/12/2022"))
+const days = ref();
+const hours = ref();
+const minutes = ref();
+const seconds = ref();
+const timeArray = ref([])
 
 </script>
 
 <template>
-hello
- 
+
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
+
