f24d4ffdeb46028f94ce61652a42926d73e40adf
Maxime <maxime.daraize@gmail.com>
2022-08-27 15:11:58 -0400

    fix: change date

--- src/components/CountDown.vue
+++ src/components/CountDown.vue
@@ -2,7 +2,7 @@
 import {ref, onMounted} from "vue";
 
 // first match of the season (habs vs toronto)
-const date = ref(new Date("10/12/2042"))
+const date = ref(new Date("10/12/2022"))
 const days = ref();
 const hours = ref();
 const minutes = ref();
